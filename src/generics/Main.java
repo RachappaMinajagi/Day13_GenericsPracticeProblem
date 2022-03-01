08b4a6e5759890748311f63e56c5f353d6554c5f
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-03-02 00:48:19 +0530

    Refactored:UsesGenericToFindMaximum

---
+++ src/generics/Main.java
@@ -0,0 +1,16 @@
+package generics;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to check max of three variables");
+		String x = "kai", y = "po", z = "che", m = "bro";
+
+		Integer a = 30, b = 39, c = 24;
+		Float p = 1.9f, q = 2.8f, r = 0.2f;
+		TestMaximum.testMaximum(x, y, z);
+		TestMaximum.testMaximum(a, b, c);
+		TestMaximum.testMaximum(p, q, r);
+	}
+}
---
+++ src/generics/TestMaximum.java
@@ -0,0 +1,29 @@
+package generics;
+
+public class TestMaximum<T extends Comparable<T>> {
+	T x, y, z;
+
+//constructor
+	public TestMaximum(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+//checking max of three integers
+	public static <T extends Comparable<T>> T testMaximum(T x, T y, T z) {
+		T max = x;// Initializing x as greater
+		if (y.compareTo(max) > 0)
+			max = y;// as of now y is greater
+
+		if (z.compareTo(max) > 0)
+			max = z;// now z is greater
+		printMax(x, y, z, max);
+		return max;
+
+	}
+
+	public static <T> void printMax(T x, T y, T z, T max) {
+		System.out.printf("the max of %s , %s and %s is %s\n", x, y, z, max);
+	}
+}
